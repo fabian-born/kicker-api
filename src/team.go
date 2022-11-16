9be0bdc74f6790b480ac7b6de4a6c745fd302920
Fabian Born <fabian.born@fabianborn.net>
2022-11-16 19:27:29 +0000

    bugfix database connect

--- src/team.go
+++ src/team.go
@@ -18,7 +18,7 @@ func ListTeams(c *gin.Context) {
 	var teamlist []Team
 
 	b_dec_cred, _ := b64.StdEncoding.DecodeString((myconf.Credential))
-	db, err := sql.Open("mysql", strings.TrimSuffix(string(b_dec_cred), "\n")+"@tcp("+myconf.DBHost+":"+myconf.DBPort+")/" + myconf.dbkicker )
+	db, err := sql.Open("mysql", strings.TrimSuffix(string(b_dec_cred), "\n")+"@tcp("+myconf.DBHost+":"+myconf.DBPort+")/" + myconf.Dbkicker )
 
 	defer db.Close()
 	rows, err := db.Query("SELECT id, name FROM kicker")
@@ -48,7 +48,7 @@ func AddTeam(c *gin.Context) {
 		return
 	}
 	b_dec_cred, _ := b64.StdEncoding.DecodeString((myconf.Credential))
-	db, err := sql.Open("mysql", strings.TrimSuffix(string(b_dec_cred), "\n")+"@tcp("+myconf.DBHost+":"+myconf.DBPort+")/" + myconf.dbkicker )
+	db, err := sql.Open("mysql", strings.TrimSuffix(string(b_dec_cred), "\n")+"@tcp("+myconf.DBHost+":"+myconf.DBPort+")/" + myconf.Dbkicker )
 
 	insert, err := db.Query("INSERT INTO teams VALUES ( NULL, '" + newTeam.Name + "' )")
 	if err != nil {
