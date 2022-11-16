1f1deed0cc2e6ac01acea627b18fa3a44e326a73
Fabian Born <fabian.born@fabianborn.net>
2022-11-16 09:17:39 +0000

    adding team functions

---
+++ src/team.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+        "database/sql"
+        b64 "encoding/base64"
+        "encoding/json"
+        "log"
+        "net/http"
+        "strings"
+
+        "github.com/gin-gonic/gin"
+        _ "github.com/go-sql-driver/mysql"
+)
+
